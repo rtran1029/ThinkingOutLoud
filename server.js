e1dda63d70151962c208ec5caa63407a20a14723
rtran1029 <103476583+rtran1029@users.noreply.github.com>
2022-09-28 23:51:18 -0400

    final run down and screenafy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 
 app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ThinkingOutLoud", {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
